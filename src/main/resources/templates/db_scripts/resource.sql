96df15f023ff9ee85a0cdda5e97a5f2f4bdbf6cb
Dazhi Jiao <djiao@jhu.edu>
2019-03-01 00:13:03 -0500

    OR-156 Created stored procedure for creating full text index for concatenated fields

---
+++ src/main/resources/templates/db_scripts/resource.sql
@@ -0,0 +1,71 @@
+CREATE OR REPLACE FUNCTION diku_mod_oriole.extract_keyword_element(input jsonb) RETURNS SETOF text AS $$
+DECLARE
+        key text;
+        value jsonb;
+BEGIN
+        CASE jsonb_typeof(input)
+                WHEN 'object' THEN
+                        FOR key, value IN SELECT * FROM jsonb_each(input) LOOP
+                          case key
+                            when 'title' then
+                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
+                            when 'term' then
+                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
+                            when 'terms' then
+                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
+                            when 'subject' then
+                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
+                            when 'description' then
+                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
+                            else
+                              null;
+                          end case;
+                        END LOOP;
+                WHEN 'array' THEN
+                        FOR value IN SELECT jsonb_array_elements(input) LOOP
+                                RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
+                        END LOOP;
+                WHEN 'string' THEN
+                        RETURN NEXT input #>> '{}';
+                ELSE
+                        NULL;
+        END CASE;
+END
+$$ LANGUAGE plpgsql;
+
+alter function diku_mod_oriole.extract_keyword_element(input jsonb) owner to folio;
+
+CREATE OR REPLACE FUNCTION diku_mod_oriole.combine_keyword_element(input jsonb) RETURNS text AS $$
+DECLARE
+  keywords text;
+BEGIN
+  select string_agg(txt, ' ') into keywords from diku_mod_oriole.extract_keyword_element(input) as txt;
+  return keywords;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE ;
+
+alter function diku_mod_oriole.combine_keyword_element(input jsonb) owner to folio;
+
+ALTER TABLE diku_mod_oriole.resource ADD column keywords tsvector;
+create index idx_keyword_full_text on diku_mod_oriole.resource using gin(keywords);
+
+create function diku_mod_oriole.resource_set_keywords()
+  returns trigger
+as $$
+BEGIN
+  NEW.keywords = to_tsvector('english', diku_mod_oriole.combine_keyword_element(NEW.jsonb));
+--   NEW.keywords = to_tsvector('English', NEW.jsonb::text);
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+alter function diku_mod_oriole.resource_set_keywords()
+  owner to folio;
+
+create trigger set_resource_keywords_trigger
+  before insert or update
+  on resource
+  for each row
+execute procedure resource_set_keywords();
+
+
