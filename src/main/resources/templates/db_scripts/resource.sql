832ea0500745864df2eeab6989bf3260e589cd86
Dazhi Jiao <djiao@jhu.edu>
2019-03-04 10:55:26 -0500

    OR-156 Upated stored procedure to use variables for schema name.

--- src/main/resources/templates/db_scripts/resource.sql
+++ src/main/resources/templates/db_scripts/resource.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION diku_mod_oriole.extract_keyword_element(input jsonb) RETURNS SETOF text AS $$
+CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.extract_text_elements(input jsonb) RETURNS SETOF text AS $$
 DECLARE
         key text;
         value jsonb;
@@ -6,24 +6,11 @@ BEGIN
         CASE jsonb_typeof(input)
                 WHEN 'object' THEN
                         FOR key, value IN SELECT * FROM jsonb_each(input) LOOP
-                          case key
-                            when 'title' then
-                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
-                            when 'term' then
-                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
-                            when 'terms' then
-                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
-                            when 'subject' then
-                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
-                            when 'description' then
-                              RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
-                            else
-                              null;
-                          end case;
+                                RETURN QUERY SELECT extract_text_elements(value);
                         END LOOP;
                 WHEN 'array' THEN
                         FOR value IN SELECT jsonb_array_elements(input) LOOP
-                                RETURN QUERY SELECT diku_mod_oriole.extract_keyword_element(value);
+                                RETURN QUERY SELECT extract_text_elements(value);
                         END LOOP;
                 WHEN 'string' THEN
                         RETURN NEXT input #>> '{}';
@@ -33,38 +20,72 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-alter function diku_mod_oriole.extract_keyword_element(input jsonb) owner to folio;
+CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.extract_keyword_element(input jsonb) RETURNS SETOF text AS $$
+DECLARE
+        key text;
+        value jsonb;
+BEGIN
+        CASE jsonb_typeof(input)
+                WHEN 'object' THEN
+                        FOR key, value IN SELECT * FROM jsonb_each(input) LOOP
+                          case key
+                            when 'title' then
+                              RETURN QUERY SELECT ${myuniversity}_${mymodule}.extract_keyword_element(value);
+                            when 'term' then
+                              RETURN QUERY SELECT ${myuniversity}_${mymodule}.extract_keyword_element(value);
+                            when 'terms' then
+                              RETURN QUERY SELECT ${myuniversity}_${mymodule}.extract_keyword_element(value);
+                            when 'subject' then
+                              RETURN QUERY SELECT ${myuniversity}_${mymodule}.extract_keyword_element(value);
+                            when 'description' then
+                              RETURN QUERY SELECT ${myuniversity}_${mymodule}.extract_keyword_element(value);
+                            else
+                              null;
+                          end case;
+                        END LOOP;
+                WHEN 'array' THEN
+                        FOR value IN SELECT jsonb_array_elements(input) LOOP
+                                RETURN QUERY SELECT ${myuniversity}_${mymodule}.extract_keyword_element(value);
+                        END LOOP;
+                WHEN 'string' THEN
+                        RETURN NEXT input #>> '{}';
+                ELSE
+                        NULL;
+        END CASE;
+END
+$$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION diku_mod_oriole.combine_keyword_element(input jsonb) RETURNS text AS $$
+-- alter function ${myuniversity}_${mymodule}.extract_keyword_element(input jsonb) owner to folio;
+
+CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.combine_keyword_element(input jsonb) RETURNS text AS $$
 DECLARE
   keywords text;
 BEGIN
-  select string_agg(txt, ' ') into keywords from diku_mod_oriole.extract_keyword_element(input) as txt;
+  select string_agg(txt, ' ') into keywords from ${myuniversity}_${mymodule}.extract_keyword_element(input) as txt;
   return keywords;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE ;
 
-alter function diku_mod_oriole.combine_keyword_element(input jsonb) owner to folio;
+-- alter function ${myuniversity}_${mymodule}.combine_keyword_element(input jsonb) owner to folio;
 
--- ALTER TABLE diku_mod_oriole.resource ADD column keywords tsvector;
--- create index idx_keyword_full_text on diku_mod_oriole.resource using gin(keywords);
+-- ALTER TABLE ${myuniversity}_${mymodule}.resource ADD column keywords tsvector;
+-- create index idx_keyword_full_text on ${myuniversity}_${mymodule}.resource using gin(keywords);
 
-create function diku_mod_oriole.resource_set_keywords()
+create function ${myuniversity}_${mymodule}.resource_set_keywords()
   returns trigger
 as $$
 DECLARE
   keywords text;
 BEGIN
---   NEW.keywords = to_tsvector('english', diku_mod_oriole.combine_keyword_element(NEW.jsonb));
-  keywords = replace(diku_mod_oriole.combine_keyword_element(NEW.jsonb), '"', ' ');
+--   NEW.keywords = to_tsvector('english', ${myuniversity}_${mymodule}.combine_keyword_element(NEW.jsonb));
+  keywords = replace(${myuniversity}_${mymodule}.combine_keyword_element(NEW.jsonb), '"', ' ');
   NEW.jsonb = jsonb_set(NEW.jsonb, '{keywords}', concat('"', keywords, '"')::jsonb, true);
 --   NEW.keywords = to_tsvector('English', NEW.jsonb::text);
   RETURN NEW;
 END;
 $$ language plpgsql;
 
-alter function diku_mod_oriole.resource_set_keywords()
-  owner to folio;
+-- alter function ${myuniversity}_${mymodule}.resource_set_keywords() owner to folio;
 
 create trigger set_resource_keywords_trigger
   before insert or update
