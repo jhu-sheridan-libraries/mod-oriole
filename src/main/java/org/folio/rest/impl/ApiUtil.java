d7253c20ef420fd54dd653a13e7dbbecc3f8df0f
Jamie Little <jamie@jamielittle.org>
2024-11-06 13:59:06 -0600

    Update ApiUtil to work with raml module builder v24.0.0

--- src/main/java/org/folio/rest/impl/ApiUtil.java
+++ src/main/java/org/folio/rest/impl/ApiUtil.java
@@ -13,22 +13,18 @@ import org.z3950.zing.cql.cql2pgjson.SchemaException;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Collections;
 
 public class ApiUtil {
     public static CQLWrapper getCQL(String query, int limit, int offset, String table, String schema)
             throws IOException, FieldException, SchemaException {
-        CQL2PgJSON cql2pgJson;
-        if (schema != null) {
-            cql2pgJson = new CQL2PgJSON(table + ".jsonb", schema);
-            //cql2pgJson = new CQL2PgJSON(RESOURCE_TABLE + ".jsonb");
-        } else {
-            cql2pgJson = new CQL2PgJSON(table + ".jsonb");
-        }
+        // Create CQL2PgJSON with just the field name
+        CQL2PgJSON cql2pgJson = new CQL2PgJSON(Collections.singletonList(table + ".jsonb"));
+
         return new CQLWrapper(cql2pgJson, query)
                 .setLimit(new Limit(limit))
                 .setOffset(new Offset(offset));
     }
-
 
     public static PostgresClient getPostgresClient(Map<String, String> okapiHeaders, Context vertxContext) {
         String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
